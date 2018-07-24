8a47e1d2e06a4c3a0fd6ac4e551cd19ebf1b4b1e
vibinggor <512464940@qq.com>
2018-07-24 13:51:26 +0800

    新增mian文件

---
+++ main.cpp
@@ -0,0 +1,21 @@
+# @Date    : 2018-07-24 01:35:36
+# @Author  : mop (you@example.org)
+# @Link    : http://example.org
+# @Version : V10
+
+#include <ostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/epoll.h>
+#include <sys/fcntl.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "hello world." << std::endl;
+	std::cout << "en en." << std::endl;
+
+	return 0;
+}
+
